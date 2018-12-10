e5280e55dd9a2a04a963f8fb5146728fee1d7b14
apjansing <apjansing@gmail.com>
2018-12-10 18:30:29 -0500

    added docker image for development

--- src/start_yarn_and_apex_docker.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd /opt/hadoop
-sbin/start-dfs.sh
-sbin/start-yarn.sh
-
-# https://hub.docker.com/r/apacheapex/sandbox/
-docker pull apacheapex/sandbox
-docker run -it --name=apex-sandbox -p 50070:50070 -p 8089:8088 apacheapex/sandbox
