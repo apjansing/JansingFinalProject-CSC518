8aea6d81645c7ae92c201fb57f00d152f06f2e29
apjansing <apjansing@gmail.com>
2018-12-10 20:19:42 -0500

    changed script to two scripts; one for building from Dockerfile and one for pulling Docker image

---
+++ src/docker/pull_and_start_dev_apex_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+JANSING_PROJECT_HOME=`(git rev-parse --show-toplevel)`
+JANSING_REPO_NAME=`(basename $JANSING_PROJECT_HOME)`
+
+docker pull apjansing/apex-maven
+docker run -it --name=apex -v $JANSING_PROJECT_HOME:/home/apex/$JANSING_REPO_NAME -p 50070:50070 -p 8089:8088 apex-maven
--- src/docker/start_yarn_and_apex_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-JANSING_PROJECT_HOME=`(git rev-parse --show-toplevel)`
-JANSING_REPO_NAME=`(basename $JANSING_PROJECT_HOME)`
-
-# https://hub.docker.com/r/apacheapex/sandbox/
-# docker pull apacheapex/sandbox
-docker build -t apex-maven .
-docker run -it --name=apex -v $JANSING_PROJECT_HOME:/home/apex/$JANSING_REPO_NAME -p 50070:50070 -p 8089:8088 apex-maven
